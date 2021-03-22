4c1930cfb3489630285d008dfc1c8633864bf025
chrystalme <afam_ifeanyi@live.com>
2021-03-22 17:32:51 +0200

    Add user model test

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,13 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: 'model' do
-  let(:user) {User.new(email: 'chrys.afam@now.com', name: 'Chrys')}
-  let(:posts_1) {Post.new(content: 'New Post body')}
-  let(:posts_2) {Post.new(content: 'Second Post body')}
-
-    describe 'association' do
-    let(:user) {User.new(email: 'chrys.afam@now.com', name: 'Chrys')}
-    it 'has many posts'
-    expect(user).to respond_to(:posts)
+    context 'association' do
+      it { should have_many (:posts) }
   end
 end
