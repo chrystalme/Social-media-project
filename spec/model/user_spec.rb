d30d1123934418a4743df18d79fdac266bba8951
chrystalme <afam_ifeanyi@live.com>
2021-03-24 09:50:53 +0200

    Add test for pending_friendships

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: 'model' do
     it { should have_many(:friendships) }
     it { should have_many(:inverse_friendships) }
     it { should have_many(:accepted_friendships) }
+    it { should have_many(:pending_friendships) }
   end
   describe 'validations' do
     it { should validate_presence_of(:name) }
