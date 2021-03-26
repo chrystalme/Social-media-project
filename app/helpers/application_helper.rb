dcc52a2786643cdbb0ed1082d0c9870d16dc8e81
chrystalme <afam_ifeanyi@live.com>
2021-03-26 12:21:55 +0200

    Move logic to helper in _user and profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,11 +16,22 @@ module ApplicationHelper
     end
   end
 
-  # def accept_or_deny_btn(friendship)
-  #   friend_request = Friendship.find_by( user: current_user , friendship: friendship)
-  #   if friend_request
-  #     link_to( 'Accept', user_friendship_path(@user, i.id))
-  #     link_to( 'Deny', user_friendships_path(@user))
-  #   end
-  # end
+  def profile_btn(user)
+    output = ''
+    if !current_user.friend?(user)
+      if current_user.pending_friends.include?(user) 
+        output << "<p class='btn btn-info disabled'> Pending </p>"
+      elsif current_user.friend_requests.include?(user) 
+        output << "#{ link_to 'Accept', user_friendship_path(current_user, current_user.pending_friendship_ids[0] ), class:'btn btn-outline-primary', method: :patch }"
+        output << "#{ link_to 'Deny', user_friendships_path(user) , class:'btn btn-outline-secondary' }"
+      else 
+        output << "<p class='px-2'>"
+        output << "#{link_to 'Invite to Friend', user_friendships_path(user, current_user), class:'btn btn-secondary', method: :post }"
+      output << "</p>"
+      end 
+    else 
+      output << "<p class='btn btn-info disabled'> Friend </p>"
+    end    
+    output.html_safe
+  end
 end
