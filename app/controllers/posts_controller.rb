01688a7387d89bd18e690bf228c2fa6b3bce0ad0
chrystalme <afam_ifeanyi@live.com>
2021-03-26 11:48:25 +0200

    Fix post order in timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
   def timeline_posts
     # @timeline_posts ||= Post.where("user_id = ?", current_user.friends ).ordered_by_most_recent.includes(:user)
     @timeline_posts = []
-    Post.all.each do |post|
+    Post.all.ordered_by_most_recent.each do |post|
       if post.user_id == current_user.id
         @timeline_posts << post
       end
